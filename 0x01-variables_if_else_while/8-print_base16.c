dcf2528231dc6f018a5e2050bb9f20a74bbcf758
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-17 12:10:28 -0700

    file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,19 @@
 
 int main(void)
 {
-	char num = '0';
+	char num = '0', ch = 'a';
 
-	while (num <= 'f')
+	while (num <= '9')
 	{
 		putchar(num);
 		num++;
 	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+
 	putchar('\n');
 	return (0);
 }
