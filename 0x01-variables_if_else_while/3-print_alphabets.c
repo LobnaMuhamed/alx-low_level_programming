aac9b5bfe1d9616943ed689569f3bfcac35dcc39
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-17 09:36:19 -0700

    file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (char <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	char ch2 = 'A';
+
+	while (chat <= 'Z')
+	{
+		putchar(ch2);
+		ch2++;
+	}
+	putchar('\n');
+	return (0);
+}
