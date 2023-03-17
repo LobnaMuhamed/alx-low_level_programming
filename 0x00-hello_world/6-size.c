29d2b21ce49a98c42885415aa047354f3b1d3180
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-16 17:59:49 -0700

    file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,18 +7,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	int a;
-	short int b;
-	long int c;
-	long long d;
-	char e;
-	float y;
-	printf("size of signed int %d", (signed long)sizeof(a));
-	printf("size of signed short int %d \n", (signed short)sizeof(b));
-	printf("size of signed long int %ld \n", (signed long)sizeof(c));
-	printf("size of signed long long int %lld \n", (signed long long)sizeof(d));
-	printf("size of unsigned char %u \n", (unsigned long)sizeof(e));
-	printf("Size of a float: %lu \n", (unsigned long)sizeof(f));
+{       int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
