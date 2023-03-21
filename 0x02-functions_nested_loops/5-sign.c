f76d742cf658c0ece9e0d3a3f4840d836fbe5759
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-21 12:31:36 -0700

    file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,21 +11,25 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n);
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
+		_putchar(',');
+                _putchar(' ');
+                _putchar(n);
 		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
+		_putchar(',');
+                _putchar(' ');
+                _putchar(n);
 		return (-1);
 	}
-	_putchar(',');
-	_putchar(' ');
-	_putchar(n);
-	else
-		return (0);
 }
