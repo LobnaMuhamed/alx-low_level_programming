fb8f78ffbb1c4d2746b0ce563f7d5ad1a81f0545
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-21 16:16:40 -0700

    file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,14 +5,16 @@
 
 void times_table(void) 
 { 
-	int row, col;
+	int row, col, result;
 
 	for (row = 0 ; row <= 9 ; row++)
 	{
 		for (col = 0 ; col <= 9 ; col++)
 		{
+			result = row + col;
 			_putchar(col + '0');
 		
 		}
+		_putchar('\n');
 	}
 }
