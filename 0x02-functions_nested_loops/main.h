90bb98b135e5197f61bd14ada42134e43b30fbeb
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-22 00:40:43 -0700

    file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ void _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+void print_times_table(int n);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
