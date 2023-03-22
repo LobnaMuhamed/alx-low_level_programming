f1e145d26698412044b011f96199c03d3982a977
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-22 01:31:28 -0700

    file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,35 +5,40 @@ void print_times_table(int n)
 {
         int row, col, result;
 
-        for (row = 0 ; row <= n ; row++)
-        {
-                for (col = 0 ; col <= n ; col++)
-                {
-                        result = row * col;
-			if (result < 9)
-			{
-				_putchar(result + '0');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(',');
-			}
-			else if (result > 9 && result < 100 )
-			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(',');
-			}
-			else if (result > 100)
-			{
-				_putchar((result / 100) + '0');
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
+	if (!(n > 15 || n < 0))
+	{	for (row = 0 ; row <= n ; row++)
+        	{
+                	for (col = 0 ; col <= n ; col++)
+                	{
+                        	result = row * col;
+				if (col != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}	
+				if (result < 10 && col != 0)
+				{
+					_putchar(result + '0');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (result >= 10 && result < 100 )
+				{
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 100 && col != 0)
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar((result % 10) + '0');
+				}
+				else 
+					_putchar((result % 10) + '0');
 			
-                }
-                _putchar('\n');
-        }
+                	}
+              	  _putchar('\n');
+		}
+	}
 }
