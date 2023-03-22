ce98fe669d6451430274e01157b6efcaf26dec21
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-22 01:20:29 -0700

    file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,22 +10,29 @@ void print_times_table(int n)
                 for (col = 0 ; col <= n ; col++)
                 {
                         result = row * col;
-			if (result > 100)
+			if (result < 9)
 			{
-				_putchar((result / 100) + '0');
+				_putchar(result + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(',');
 			}
-			else if (result > 9)
-                                _putchar((result / 10) + '0');
-			else if (result <= 9 && col != 0)
-			{ 	_putchar(' ');
-                	}       
-			_putchar((result % 10) + '0');
-                        if (col != n)
-                        {
-                                _putchar(',');
-                          	_putchar(' ');
-			 	_putchar(' ');
-                        }
+			else if (result > 9 && resilt < 100 )
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(',');
+			}
+			else if (result > 100)
+			{
+				_putchar((rsult / 100) + '0');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			
                 }
                 _putchar('\n');
         }
