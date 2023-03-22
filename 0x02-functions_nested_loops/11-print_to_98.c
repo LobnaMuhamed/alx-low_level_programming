148ede8c2ca09b5782ea67dc8982b1b4b3035caa
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-21 17:25:01 -0700

    file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+	int num;
+	for (num = n ; num <= 98 ; num++)
+	{
+		if (num > 9)
+			_putchar((num / 10) + '0');
+		_putchar((num % 10) + '0');
+		if (num < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
