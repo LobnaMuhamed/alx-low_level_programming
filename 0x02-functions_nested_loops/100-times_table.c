86f5c9e393b598d166a3abcf2116d6c36e618e17
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-22 00:45:59 -0700

    file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,8 @@ void print_times_table(int n)
                         if (col != n)
                         {
                                 _putchar(',');
-                                _putchar(' ');
+                          	_putchar(' ');
+			 	_putchar(' ');
                         }
                 }
                 _putchar('\n');
