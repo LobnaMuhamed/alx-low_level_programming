4f6d10359d96a7c198b7558fa44c840b6ac3d8a2
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-23 16:30:55 -0700

    file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,19 +7,24 @@
 
 void print_diagonal(int n)
 {
-        if (n < 0 || n == 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                int i;
+	if (n < 0 || n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
 
-                for (i = 0 ; i < n ; i++)
-                {
-                        _putchar('\');
-                }
-                _putchar('\n');
-        }	
+		for (i = 0 ; i < n ; i++)
+		{
+			int j;
 
+			for (j = 0 ; j < i - 1 ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
 }
