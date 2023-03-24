2e0a790ff42f7155e0929607981d1498ba0dbb4b
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-23 18:35:53 -0700

    file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 1 ; i <= size ; i++)
+	for (i = 0 ; i <= size ; i++)
 	{
 		for (j = size - i ; j > 0 ; j--)
 		{
@@ -21,6 +21,7 @@ void print_triangle(int size)
 		{
 			_putchar('#');
 		}
+		_putchar(' ');
 		_putchar('\n');
 	}
 }
