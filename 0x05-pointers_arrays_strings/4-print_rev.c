107965069ae31029342a0691d9e9f00d11282e7c
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-27 16:09:25 -0700

    file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev -  a function that prints a string, in reverse
+ * @s : character input
+*/
+
+void print_rev(char *s)
+{
+
+	int i, length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (i = length ; i > 0 ; i--)
+	{
+		_putchar(s[i]);
+	}	
+	_putchar('\n');
+}
