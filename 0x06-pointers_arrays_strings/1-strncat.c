dee7ae532e02a360c16a99f31f524c8cf6a3421a
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-03-29 01:52:45 -0700

    file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,21 +5,23 @@
  * @dest: input value
  * @src: input value
  * @n: input value
+ * Return: Void
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
-        while (src[j] != '\0' && j < n)
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
-        dest[i] = '\0';
-        return (dest);
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
