9f40dd11aee12635bab7e2f37548b0b7caca8fca
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-03 08:43:43 -0700

    file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - a function that fills memory with a constant byte
- * @s : input value
- * @b : input value
- * @n : input value
- * Return : return value of s pointer
+ * *_memset - a function that fills memory with a constant byte
+ * @s: input value
+ * @b: input value
+ * @n: input value
+ * Return: return value of s pointer
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for ( i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		s[i] = b;
 	}
