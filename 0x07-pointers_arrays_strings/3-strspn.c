80a74ec17fbc282f0167fcbcf8ad4c9f1b0e292a
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-03 15:13:17 -0700

    file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,15 +9,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, count = 0;
+	int i, j;
+       	unsigned int count = 0;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	for (i = 0 ; s[i] >= '\0' ; i++)
 	{
-		for (j = 0 ; accept[j] != '\0' ; j++)
+		for (j = 0 ; accept[j] >= '\0' ; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				count++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+			{
+				return (count);
+				break;
 			}
 		}
 	}
