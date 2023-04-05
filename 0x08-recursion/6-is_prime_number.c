deba361a138a81c74a10cd176da357fc79534319
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-05 16:35:15 -0700

    file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return(prime_number(n, n - 1));
+	return (prime_number(n, n - 1));
 }
 
 /**
@@ -26,5 +26,5 @@ int prime_number(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return(prime_number(n, i - 1));
+	return (prime_number(n, i - 1));
 }
