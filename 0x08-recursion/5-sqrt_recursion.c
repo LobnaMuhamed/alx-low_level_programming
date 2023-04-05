89cd5fa543304840a9319dd88ace669cc7069092
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-05 16:48:51 -0700

    file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int f_sqrt(int n, int i);
+
 /**
  * _sqrt_recursion - a function that returns the natural square
  * @n: input value
@@ -12,6 +14,20 @@ int _sqrt_recursion(int n)
 		return (-1);
 	else 
 	{
-		return (_sqrt_recursion(n - 1) + n + n - 1);
+		return (f_sqrt(n, 0));
 	}
+}
+/**
+ * f_sqrt - a function that recursive number
+ * @n: input value
+ * @i: input value
+ * Return: return value
+*/
+int f_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (f_sqrt(n , i + 1));
 }
