8104faa828c8a97da52f2a3dd9560c1ceb3e5987
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-05 16:51:59 -0700

    file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,10 +12,8 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else 
-	{
+	else
 		return (f_sqrt(n, 0));
-	}
 }
 /**
  * f_sqrt - a function that recursive number
@@ -29,5 +27,5 @@ int f_sqrt(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (f_sqrt(n , i + 1));
+	return (f_sqrt(n, i + 1));
 }
