022417f326856be05674e99c610441a6b2af6c9f
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-05 14:32:26 -0700

    file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,16 @@
 
 int factorial(int n)
 {
-	if (n < 0 )
+	if (n < 0)
 	{
 		return (-1);
 	}
-	if(n == 0)
+	if (n == 0)
 	{
-		return (0);
+		return (1);
 	}
-	return (n * factorial(n - 1));
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
