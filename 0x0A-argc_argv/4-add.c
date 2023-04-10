9d9848062d00ecdc7dab9a1b4cfd66de6f88c546
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-10 16:44:48 -0700

    File

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @argc: input value
+ * @argv: input value
+ * Return: return 0 (successful)
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, c;
+
+	for (i = 1 ; i < argc ; i++)
+	{
+		if (!(isdigit(argv[i])))
+		{
+			printf("Error\n");
+			return(1);
+		}
+		else 
+		{
+			c = atoi(argv[i]);
+			sum += c;	
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
