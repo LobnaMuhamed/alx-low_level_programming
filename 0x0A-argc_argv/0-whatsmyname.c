f01e25acb20321cc9326db82ad045109ab1365ec
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-10 15:34:15 -0700

    file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: input value
+ * @argv: input value
+ * Return: Always 0 (Success)
+*/
+
+int main (int argc, char *argv[] )
+{
+	printf("%s", argv[0]);
+	return (0);
+}
