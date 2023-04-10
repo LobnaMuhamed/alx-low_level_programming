de8295425ed018aca1c11b4558659387bfb58da3
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-10 15:46:31 -0700

    file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: input value
+ * @argv: input value
+ * Return: return 0 (Successful)
+*/
+
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+		return (0);
+}
