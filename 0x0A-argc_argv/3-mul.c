64dfff02e324e5c61a04b58b0340e8a40741cfa5
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-10 16:14:37 -0700

    file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,8 +12,16 @@
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	int res = 0;
-	
-	res = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", res);
+
+	if (argc == 3)
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
