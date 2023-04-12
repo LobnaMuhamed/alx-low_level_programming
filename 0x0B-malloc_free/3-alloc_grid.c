5c603a63f5ded7a91b0022fa8d356be3d38ba070
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-11 19:21:04 -0700

    file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include "main.h"i
+#include <stdlib.h>
+
+/**
+ * alloc_grid - nested loop
+ * @width: width input value
+ * @height: height input value
+ * Return: return pointer
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	int **arr;
+
+	if(width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int *) * height);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < height ; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+
+		if (arr[i] == NULL)
+		{
+			for (; i > 0 ; i--)
+				free(arr[i]);
+			
+			
+			free(arr);
+			
+			return (NULL);
+		}
+	}
+	for (i = 0 ; i < height ; x++)
+	{
+		for (j = 0 ; j < width ; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+
+
+}
