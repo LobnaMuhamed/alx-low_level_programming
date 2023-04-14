424326fc4a7cabbf5a1ef226dbe38055e48b323b
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-13 19:25:55 -0700

    file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,25 +16,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ch;
 
 	while (s1 && s1[len1])
-	{
 		len1++;
-	}
 	while (s2 && s2[len2])
-	{
 		len2++;
-	}
 	if (n < len2)
-	{
 		ch = malloc(len1 + n + 1);
-	}
 	else
-	{
 		ch = malloc(len1 + len2 + 1);
-	}
 	if (ch == NULL)
-	{
 		return (NULL);
-	}
 	while (i < len1)
 	{
 		ch[i] = s1[i];
@@ -53,7 +43,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		j++;
 	}
 	ch[i] = '\0';
-
 	return (ch);
-
 }
