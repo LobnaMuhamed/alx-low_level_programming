7dbaf4ee1c4da1edea689958d9de98bd8d623771
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-15 16:10:56 -0700

    File

--- 0x02-functions_nested_loops/1-alphabet_O.c
+++ 0x02-functions_nested_loops/1-alphabet_O.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet
-*/
+ * print_alphabet - print all alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
 
 void print_alphabet(void)
 {
