8e3b08cc62a191d530d352b9fe0d7631fb8ce534
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-16 16:26:12 -0700

    file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,13 +5,21 @@
  * Return: return 0 (Successful)
 */
 
+
 int main(void)
 {
-	int x;
+	int i, x, j, y;
 
-	for (x = 0 ; x < 50 ; x++)
+	x = 0;
+	j = 1;
+	printf("%d ", x);
+	printf("%d ", j);
+	for (i = 3 ; i <= 50 ; ++i)
 	{
-		printf("%d, ", x + x);
+		y = x + j;
+		printf("%d ", y);
+		x = j;
+		j = y;
 	}
 	return (0);
 }
