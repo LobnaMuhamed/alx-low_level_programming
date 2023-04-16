bac6230f4fa54c860db0c10533314ccf826b6837
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-15 17:15:22 -0700

    file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, 1");
+		printf("+");
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-, /");
+		printf("-");
 		return (-1);
 	}
 	else if (n == 0)
 	{
-		printf("0, 0");
+		printf("0");
 		return (0);
 	}
 	return (0);
