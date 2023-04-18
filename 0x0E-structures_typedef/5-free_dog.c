b84a6db10d17449f1b5f08b2e26f75ff956c9241
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-17 18:26:17 -0700

    file

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "main.h"
+#include "dog.h"
 
 /**
  * free_dog -  a function that frees dogs.
@@ -12,7 +12,6 @@ void free_dog(dog_t *d)
 	{
 	free(d->owner);
 	free(d->name);
-	free(d->age);
 	free(d);
 	}
 }
