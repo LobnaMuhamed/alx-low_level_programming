d65725b900f49b12c130cac032da073190cd0f5a
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-19 19:37:38 -0700

    file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array elem on a newl
+ * @array: input value
+ * @size: input value
+ * @action: input value
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (x = 0; x < size; x++)
+	{
+		action(array[x]);
+	}
+}
+
