7ad1154c83190d1f7fbf0dc2c56b9a66c4ccbe80
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-27 17:49:33 -0700

    file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,18 @@
 #include "lists.h"
 
+/**
+ * list_len -  function that returns the number of elements in a linked 
+ * @h: head pointer
+ * Return: return n
+*/
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
 	while (h)
 	{
-		h = h-> next;
 		n++;
+		h = h-> next;
 	}
 	return (n);
 }
