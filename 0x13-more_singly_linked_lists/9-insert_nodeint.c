b60a34062270ad0b8c46af3755a260846f79f4a5
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-01 17:14:26 +0300

    File

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,10 +16,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 
 	new->n = n;
-
+	new->next = NULL;
 	if (head == NULL || new == NULL)
 		return (NULL);
-	
+
 	if (idx == 0)
 	{
 		new->next = *head;
