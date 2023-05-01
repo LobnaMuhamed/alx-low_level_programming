fd8af7311c55fe0db95eded2c7302aba7750fda9
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-01 16:56:23 +0300

    File

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,7 @@
 
 /**
  * insert_nodeint_at_index - a function that insert a new node
+ * @head: a pointer for the first element
  * @idx: the index of the list
  * @n: input value
  * Return: return address of new node or NULL
@@ -18,7 +19,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (*head == NULL || new == NULL)
 		return (NULL);
-
+	
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new)
+	}
 	for (i = 0 ; i < idx - 1 && temp ; i++)
 	{
 		temp = temp->next;
