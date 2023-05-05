702951d5bf7915a381f2a719135286d85edb42da
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-05 12:20:17 -0700

    create 100-firsrt.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main - print a function before execute main functio
+*/
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
