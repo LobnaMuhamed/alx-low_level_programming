9cac2dc7ff20f7ca83b730cd4d22ff8c8a36e2a2
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-04 17:12:54 -0700

    betty main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
-#include <stdio.h>
-#include <math.h>
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <math.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
