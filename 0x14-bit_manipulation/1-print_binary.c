8940aceff4a06dd9588b8de17db60bdaf96677f2
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-04 18:17:54 -0700

    create 1-print_bunary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * pown - get 2 raised to the power i
+ * @num: input value
+ * @i: input value
+ * Return: return the result
+*/
+
+unsigned int pown(unsigned int num, unsigned int i)
+{
+	unsigned int j = 0, result = 1;
+
+	while (j < i)
+	{
+		result *= num;
+		j++;
+	}
+	return (result);
+}
+
+/**
+ * print_binary -  a function that prints the binary representation of a number
+ * @n: input value
+*/
+
+void print_binary(unsigned long int n)
+{
+	int i, num_bits = 0;
+	unsigned long int num, temp = n;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+
+	while (temp > 0)
+	{
+		temp >>= 1;
+		num_bits++;
+	}
+	for (i = num_bits - 1 ; i >= 0 ; i--)
+	{
+		num = pown(2, i);
+		if (n >= num)
+		{
+			_putchar('1');
+			n -= num;
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
