0952ef4825c480e5ba27be27fa5237e44acafa87
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-10 21:28:06 -0700

    File 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints it
+ * @filename: file should be read
+ * @letters: the number of letters it should read and print
+ * Return: return actual number of letters
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	ssize_t r_bytes = 0, w_bytes = 0;
+	char ch[1024];
+
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (r_bytes <= (ssize_t)letters && fgets(ch, letters, fp) != NULL)
+		{
+			w_bytes = write(1, ch, strlen(ch));
+			if (w_bytes != (ssize_t)strlen(ch))
+				return (0);
+			r_bytes += strlen(ch);
+		}
+
+		fclose(fp);
+		return (r_bytes);
+	}
+}
