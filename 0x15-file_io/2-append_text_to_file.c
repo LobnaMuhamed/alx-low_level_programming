ee4e36057d3c1385e34b3deba29a21298edf58c8
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-11 06:03:49 -0700

    Create 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	else
+	else if (text_content != NULL)
 	{
 		if (dprintf(fileno(fptr), "%s", text_content) < 0)
 		{
