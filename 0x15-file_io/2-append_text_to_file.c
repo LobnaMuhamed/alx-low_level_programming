09c7d894b772e71fb6a47de4d3e821be3604ff11
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-11 06:05:18 -0700

    Create 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,9 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			fclose(fptr);
 			return (-1);
 		}
-		if (chmod(filename, S_IRUSR || S_IWUSR) == -1)
-			return (-1);
-		fclose(fptr);
 	}
+	fclose(fptr);
 	return (1);
 }
