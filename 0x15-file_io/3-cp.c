d5029f03a49fabd58858eac11d3a97f52ed31d24
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-13 09:58:57 -0700

    fix 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
 		print_error("Error: Can't read from file", argv[1], 98, buffer);
 	r_bytes = write(file_to, buffer, r_bytes);
 	if (r_bytes == -1)
-		print_error("Error: Can't write to", arg[2], 99, buffer);
+		print_error("Error: Can't write to", argv[2], 99, buffer);
 	if (close(file_from) == -1)
 		print_error("Error: Can't close fd", strerror(errno), 100, buffer);
 	if (close(file_to) == -1)
