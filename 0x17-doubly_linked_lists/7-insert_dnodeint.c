bc02e1fa263345e4820cb2a9d9476cf966e8241a
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-15 23:15:15 -0700

    fix task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp, *new_node;
+	dlistint_t *temp, *new_node = NULL;
 	unsigned int i;
 
 	if (idx == 0)
