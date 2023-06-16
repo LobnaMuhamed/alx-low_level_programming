a5d6d145480785599abf9513708207057562bd1e
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-16 00:07:01 -0700

    solve task

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	while (temp && i++ < index)
+	while (temp)
 	{
 		if (i == index)
 		{
@@ -32,7 +32,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (1);
 		}
 		else
+		{
+			i++;
 			temp = temp->next;
+		}
 	}
 	return (-1);
 }
