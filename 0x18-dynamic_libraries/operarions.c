61324773d8be1c822fe78c836228609be49096d4
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-19 11:17:38 -0700

    solve task 3

---
+++ 0x18-dynamic_libraries/operarions.c
@@ -0,0 +1,55 @@
+/**
+ * add - addition 2 numbers
+ * @a: input int
+ * @b: input int
+ *
+ * Return: return a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substract 2 numbers
+ * @a: input int
+ * @b: input int
+ *
+ * Retutn: return a - b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplicat 2 numbers
+ * @a: input int
+ * @b: input int
+ *
+ * Return: return 1 * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divition 2 numbers
+ * @a: input int
+ * @b: input int
+ *
+ * Return: return a / b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - module 2 numbers
+ * @a: input int
+ * @b: input int
+ *
+ * Return: return a % b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
